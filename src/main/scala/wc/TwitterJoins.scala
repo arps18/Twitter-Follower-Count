e61dda556055806d748dc602530f2927d2021f25
arps <arpanp99@gmail.com>
2024-02-15 18:44:34 -0500

    Resolved Dataset Bug

--- src/main/scala/wc/TwitterJoins.scala
+++ src/main/scala/wc/TwitterJoins.scala
@@ -1,44 +1,79 @@
+import org.apache.log4j.LogManager
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.sum
 
 object TwitterJoinsMain {
-  def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder()
-      .appName("SparkPrograms")
-      .getOrCreate()
+  def main(args: Array[String]) {
+    val logger: org.apache.log4j.Logger = LogManager.getRootLogger
+    if (args.length != 2) {
+      logger.error("Usage:\nMain <input dir> <output dir>")
+      System.exit(1)
+    }
+    val conf = new SparkConf().setAppName("DSet")
+    val sc = new SparkContext(conf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    import sqlContext.implicits._
 
-    val sc = spark.sparkContext
+    val textFile = sc.textFile(args(0))
 
+    // RDD-A
+    val addToCount = (count1: Int, count2: Int) => count1 + count2
+    val sumPartitionCount = (p1: Int, p2: Int) => p1 + p2
 
-    val df = spark.read.option("header", "true").csv("input/Twitter-dataset/data/edges.csv")
-    val edgesRDD = df.rdd.map(row => (row.getString(0), row.getInt(1)))
+    val followerCountRDDA = textFile.map(line => {
+      val users = line.split(",")
+      val followedUser = users(1)
+      (followedUser, 1)
+    }).aggregateByKey(0)(addToCount, sumPartitionCount)
 
+    println(followerCountRDDA.toDebugString)
+    followerCountRDDA.saveAsTextFile(args(1) + "/RDD-A")
 
-    val groupedRDD = edgesRDD.groupByKey()
-    val rddGResult = groupedRDD.mapValues(_.sum).collect()
-    println("RDD-G Result:")
-    rddGResult.foreach(println)
+    // RDD-F
+    val followerCountRDDF = textFile.map(line => {
+      val users = line.split(",")
+      val followedUser = users(1)
+      (followedUser, 1)
+    }).foldByKey(0)((x,y) => x+y)
 
+    println(followerCountRDDF.toDebugString)
+    followerCountRDDF.saveAsTextFile(args(1) + "/RDD-F")
 
-    val rddRResult = edgesRDD.reduceByKey(_ + _).collect()
-    println("\nRDD-R Result:")
-    rddRResult.foreach(println)
+    // RDD-G
+    val followerCountRDDG = textFile.map(line => {
+      val users = line.split(",")
+      val followedUser = users(1)
+      (followedUser, 1)
+    }).groupByKey().mapValues(noOfFollowers => noOfFollowers.reduce((x,y) => x+y))
 
+    println(followerCountRDDG.toDebugString)
+    followerCountRDDG.saveAsTextFile(args(1) + "/RDD-G")
 
-    val rddFResult = edgesRDD.foldByKey(0)(_ + _).collect()
-    println("\nRDD-F Result:")
-    rddFResult.foreach(println)
+    // RDD-R
+    val followerCountRDDR = textFile.map(line => {
+      val users = line.split(",")
+      val followedUser = users(1)
+      (followedUser, 1)
+    }).reduceByKey((x,y) => x+y)
 
+    println(followerCountRDDR.toDebugString)
+    followerCountRDDR.saveAsTextFile(args(1) + "/RDD-R")
 
-    val zeroValue = 0
-    val rddAResult = edgesRDD.aggregateByKey(zeroValue)(_ + _, _ + _).collect()
-    println("\nRDD-A Result:")
-    rddAResult.foreach(println)
+    // Dataset
+    val rdd = textFile.map(line => {
+      val users = line.split(",")
+      val followedUser = users(1)
+      (followedUser, 1)
+    })
 
+    val sparkSession =  SparkSession.builder().getOrCreate()
+    val dataset =  sparkSession.createDataset(rdd)
 
-    val dsetResult = edgesRDD.groupByKey().mapValues(_.sum).collect()
-    println("\nDSET Result:")
-    dsetResult.foreach(println)
+    val counts = dataset.groupBy("_1").agg(sum($"_2"))
+    counts.rdd.saveAsTextFile(args(1) + "/DSet")
 
-    spark.stop()
+    println(counts.explain(true))
   }
+
 }
